34f10fd9619d243ee8533f243eea1ddbcf80b735
afaysalk <91470600+afaysalk@users.noreply.github.com>
2023-02-21 17:35:04 +0200

    🚩 Incorporated different 3D models inside Scene

--- 3D-tamagotchi/src/Scene.tsx
+++ 3D-tamagotchi/src/Scene.tsx
@@ -1,22 +1,10 @@
 import {Canvas, useFrame} from "@react-three/fiber"
 import {useRef} from "react"
 import {Mesh} from "three"
-
-
-
-function Device() {
-    const deviceRef = useRef<Mesh>(null!)
-    useFrame(() => {
-        deviceRef.current.rotation.x += 0.005;})
-
-    return (
-            <mesh ref={deviceRef}>
-                <boxGeometry args={[1, 1, 1]} />
-                <meshStandardMaterial color="orange" />
-            </mesh>
-    );
-};
-
+import {Body} from "./Body"
+import {Button_left} from "./Button_left"
+import {Button_right} from "./Button_right"
+import {Button_middle} from "./Button_middle"
 
 
 function ThreeScene(){
@@ -24,7 +12,10 @@ function ThreeScene(){
         <Canvas>
            <ambientLight />
            <pointLight position={[5, 5, 5]}/>
-           <Device />
+           <Body/>
+           <Button_left/>
+           <Button_middle/>
+           <Button_right/>
         </Canvas>
     )
 
